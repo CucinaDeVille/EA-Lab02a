d2df412042b7d4d36eb0bf227b27ce47f2773be5
luca <luca.romano@t-online.de>
2026-04-16 19:16:02 +0200

    feat(calculator): :boom: add calculation to convert seconds into alternative format

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,31 @@ import AddButton from "./AddButton.jsx";
 export default function App() {
 
     // hold current value of variable
-    const [count, setCount] = useState(0);
+    const [timeInSec, setTimeInSec] = useState(0);
 
     // update value accordingly - if called by AddButton
     const add = (value) => {
-        setCount (count + value);
-        console.log(count);
+        setTimeInSec (timeInSec + value);
+        console.log(timeInSec);
     }
+
+    let hour = Math.floor(timeInSec / 3600);
+    let minute = Math.floor((timeInSec % 3600) / 60);
+    let second = Math.floor(timeInSec % 60);
 
     // display counter and call AddButton function
     // pass along increment steps and REFERENCE (!!!) to callback function
     return (
         <div className="App">
-            <p>Count: {count}</p>
-            <AddButton myInc={ 11 } myCallback = { add }/>
+            <p>Seconds: {timeInSec}</p>
+
+            <AddButton myInc={ 1 } myCallback = { add }/>
+            <AddButton myInc={ 10 } myCallback = { add }/>
+            <AddButton myInc={ 100 } myCallback = { add }/>
+            <AddButton myInc={ 1000 } myCallback = { add }/>
+            <AddButton myInc={ 10000 } myCallback = { add }/>
+
+            <p>{hour}h {minute}m {second}s</p>
         </div>
     );
 }
