546099d82e03fe577adacdaa1499090ec71930b3
luca <luca.romano@t-online.de>
2026-04-17 10:20:34 +0200

    feat(button): :bulb: add explainatory comments

--- src/AddButton.jsx
+++ src/AddButton.jsx
@@ -8,6 +8,9 @@ export default function AddButton({myInc, myCallback}) {
     }
 
     // define button with corresponding callback function
+    // onClick expects pointer to function; workaround by providing a
+    // reference to a function that calls the callback function and passes along a value
+    // onClick ALWAYS expects a reference to a function and never calls a function itself
     return (
         <button className="myButton" onClick={handleClick}>{myInc}</button>
     );
